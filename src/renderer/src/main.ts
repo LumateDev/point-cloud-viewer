b08d9a08a33703e67338dc86df66a3c6f04ba36d
emelyanenkoaa <alex12332@list.ru>
2025-12-29 16:36:02 +0300

    add ru local

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -7,11 +7,20 @@ import '@renderer/assets/scss/_base.scss';
 import 'normalize.css';
 import router from './router';
 import App from './App.vue';
+import ru from 'element-plus/es/locale/lang/ru';
+import 'dayjs/locale/ru';
+import dayjs from 'dayjs';
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(ElementPlus);
 app.use(router);
+
+app.use(ElementPlus, {
+  locale: ru,
+});
+
+dayjs.locale('ru');
 
 app.mount('#app');
